4ee6a9d4ce308c906bb87e4e7b89e9390062ccaf
Hamit Can SAYILGAN <hamitcansayilgan@gmail.com>
2024-03-13 00:26:03 +0300

    - Bugfix: Delete record button color is changed

--- frmKayitListele.Designer.cs
+++ frmKayitListele.Designer.cs
@@ -57,7 +57,7 @@
 			// 
 			// button1
 			// 
-			this.button1.BackColor = System.Drawing.Color.CadetBlue;
+			this.button1.BackColor = System.Drawing.Color.Red;
 			this.button1.Location = new System.Drawing.Point(210, 354);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(135, 46);
